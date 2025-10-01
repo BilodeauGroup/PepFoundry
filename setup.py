da6a76c481ebaf9a4f0e68d79356f0594fd4166e
danielgarzonotero <vvd9fd@virginia.edu>
2025-10-01 15:23:39 -0400

    setup

--- setup.py
+++ setup.py
@@ -4,26 +4,33 @@ try:
     with open("README.md", "r", encoding="utf-8") as f:
         long_description = f.read()
 except FileNotFoundError:
-    long_description = "This library is to obtain the RDKit molecule from the amino acid sequence including the non-natural ones present in the library"
+    long_description = (
+        "This library is used to obtain RDKit molecule objects from amino acid sequences, "
+        "including non-natural amino acids present in the library."
+    )
 
 setup(
-    name="pepfoundry", 
-    version="1.0",  
-    packages=find_packages(),  
-    install_requires=[  
+    name="pepfoundry",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
         "pandas>=1.3.5",
         "rdkit>=2023.3.2",
+        "torch>=1.13.1",
+        "torchvision>=0.14.1",
+        "openpyxl>=3.1.3",
+        "scikit-learn>=1.0.2"
     ],
-    include_package_data=True, 
+    include_package_data=True,
     package_data={
-        "pepfoundry": ["project/core/amino_acid_library.xlsx"]
+        "pepfoundry": ["project/core/amino_acids_library.xlsx"]
     },
     author="Daniel Garzon Otero",
     author_email="vvd9fd@virginia.edu",
-    description="A module to obtain the Peptide RDKit molecule from an amino acid sequence",
+    description="A module to obtain peptide RDKit molecule objects from amino acid sequences",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/BilodeauGroup/PepFoundry.git",
+    url="https://github.com/BilodeauGroup/PepFoundry",
     python_requires='>=3.7.16',
 )
 
